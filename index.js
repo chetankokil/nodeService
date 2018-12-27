4e774f1d18f7a0950df7e90b76673031e4293143
chetankokil <All189@#>
2018-12-27 15:01:52 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+
+
+
+const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes/note_routes')(app, {});
+app.listen(port, ()=> {
+    console.log("Listening on port "+port);
+});
