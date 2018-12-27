04f26c88372618177eb600c5e01e176113807f0e
chetankokil <All189@#>
2018-12-27 17:29:08 -0600

    New commit

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,10 @@
+const person = require('../models/Person')
+
 module.exports = function(app, db) {
     app.post("/notes", (req, res) => {
-        res.send("Hello");
+//        var person = new Person();
+        person.name = "CHetan";
+        person.age = "35";
+        res.send(JSON.stringify(person));
     });
 };
